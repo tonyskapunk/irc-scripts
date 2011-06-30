8e358a3bb00a8079bef13c059b092a533bf64785
GaRaGeD <garaged@gmail.com>
2011-06-29 19:00:18 -0700

    the sugar is back !

--- coffee.pl
+++ coffee.pl
@@ -17,8 +17,12 @@ $VERSION = "1.0";
 sub coffee {
   my ($server, $msg, $nick, $address, $target) = @_;
   my $instructions = "Boil water, serve it in a cup(better if clean), add instant coffe(yeah, low budget), add sugar, stir a bit(better if you do the last 3 steps with a spoon), sip it, burns? is ok, enjoy!  Damn easy, even humans can do it!!";
+  my $make_tea = "a nice glass of iced tea is comming your way, would you like some fritos or rancheritos with it ? ";
   if ( $msg =~ /c0ff33/i ) {
     $server->command('msg '.$target.' '.$nick.': '.$instructions); 
+  }
+  elsif ( $msg =~ /n3st34/i ) {
+    $server->command('msg '.$target.' '.$nick.': '.$make_tea); 
   }
   else {
     return 0;
